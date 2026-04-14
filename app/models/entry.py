15d3f7aba2cded094d14ff369ff79ae04c3dfb3e
Airstriker123 <114228623+Airstriker123@users.noreply.github.com>
2026-04-15 01:36:18 +1000

    fix table error
    fix entry table type

--- app/models/entry.py
+++ app/models/entry.py
@@ -4,14 +4,14 @@ from typing import Any
 class Entry(db.Model):
     """Table to record user entries id = pk relating what entry belongs to which user."""
     __tablename__: str = 'entries'
-    id: int = db.Column(db.Integer, primary_key=True)
-    merchant: str = db.Column(db.String(100))
+    id: Any = db.Column(db.Integer, primary_key=True)
+    merchant: Any = db.Column(db.String(100))
     date: Any = db.Column(db.DateTime)
-    amount: float = db.Column(db.Float)
-    tax: float = db.Column(db.Float)
-    category: str = db.Column(db.String(50))
-    description: str = db.Column(db.Text)
-    user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'))
+    amount: Any = db.Column(db.Float)
+    tax: Any = db.Column(db.Float)
+    category: Any = db.Column(db.String(50))
+    description: Any = db.Column(db.Text)
+    user_id: Any = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 
