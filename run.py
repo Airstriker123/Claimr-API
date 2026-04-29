1a4a059aa3d5188b1245661fef4d64fb43acfca0
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2026-04-29 14:17:54 +1000

    ++ updated dependencies
    ++ flask session

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import os
 
-class Banner(object):
-    """
-    class stores look of banner
-    - cosmetic for terminal does nothing but display server text development
-    """
 
-    def __init__(self) -> None:
-        self.banner: str = \
+def banner() -> str:
+    banner: str = \
 r"""
 __________                __                      .___    _____ __________.___ 
 \______   \_____    ____ |  | __ ____   ____    __| _/   /  _  \\______   \   |
@@ -16,24 +11,15 @@ __________                __                      .___    _____ __________.___
  |______  /(____  /\___  >__|_ \\___  >___|  /\____ |  \____|__  /____|   |___|
         \/      \/     \/     \/    \/     \/      \/          \/              
 """
-
-        self.faded_banner:str = Banner.purplepink(self.banner)
-
-    @staticmethod
-    def purplepink(text) -> str:
-        """
-        method to print a banner gradient purple gradient in this case
-        """
-        faded: str = ""
-        red: int = 40
-        for line in text.splitlines():
-            faded += f"\033[38;2;{red};0;220m{line}\033[0m\n"
-            if not red == 255:
-                red += 15
-                if red > 255:
-                    red = 255
-        return faded
-
+    faded_banner: str = ""
+    red: int = 40
+    for line in banner.splitlines():
+        faded_banner += f"\033[38;2;{red};0;220m{line}\033[0m\n"
+        if not red == 255:
+            red += 15
+            if red > 255:
+                red = 255
+    return faded_banner
 
 def start() -> bool:
     """run application"""
@@ -46,7 +32,7 @@ def start() -> bool:
     app: app = application()
     if __name__ == "__main__":
         #dev configs
-        print(Banner().faded_banner)  # print banner
+        print(banner())  # print banner
         app.run(
             debug=True,
             host="0.0.0.0",
